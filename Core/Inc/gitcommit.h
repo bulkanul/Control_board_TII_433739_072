c0329497cbceb508ddb52436994c342b9fe062d7
bulkanul <anton.segnet@gmail.com>
2026-08-05 13:11:49 +0300

    bypass clock source

--- Core/Inc/gitcommit.h
+++ Core/Inc/gitcommit.h
@@ -1,2 +1,2 @@
 #define GIT_INFO_PRESENT
- static const char* GIT_INFO = "Version Information=[1118c0ea66fcb3d67c7d18827c8bde8de9d67439, (HEAD -> master, origin/master)]\r\n";
+ static const char* GIT_INFO = "Version Information=[57805a8926e9338bfe26528560cdf49910f34e1d, (HEAD -> master, origin/master)]\r\n";
